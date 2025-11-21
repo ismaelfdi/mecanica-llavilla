404fd49167b6417991a9a6b8ae3b947f528a0719
iventura <ventura.ismael@gmail.com>
2025-11-21 00:00:22 +0000

    feat: Implementacion de Spatie/Laravel-Permission y migraciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,16 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles; // <-- 1. IMPORTA ESTA LÍNEA
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles; // <-- 2. AÑADE HasRoles AQUÍ
 
     /**
      * The attributes that are mass assignable.
